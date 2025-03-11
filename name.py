2ab3153132b37b3237d55e12a0f72a137829af43
sujith9493 <sujithkodali@gmail.com>
2025-03-11 00:57:02 -0700

    Add files via upload

---
+++ name.py
@@ -0,0 +1,10 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(hello world)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> 
+>>> print("hello world")
+hello world
+>>> 
+============================================= RESTART: C:/Users/KGRCET/AppData/Local/Programs/Python/Python312/name.py ============================================
+sujith.py
